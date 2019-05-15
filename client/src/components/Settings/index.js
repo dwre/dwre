68b68354875cfe42c9118675f36f5e2c6e34f848
Egor Rumyantsev <brewbaker@yandex.com>
2019-05-15 17:00:21 +0300

    Update index.js

--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -16,7 +16,7 @@ class Settings extends Component {
             <div className="form-check">
               <label className="form-check-label" htmlFor="sound-control">
                 <input id="sound-control" onChange={this.handleSoundToggle.bind(this)} className="form-check-input" type="checkbox" checked={this.props.soundIsEnabled} />
-                Sound
+                Звук
               </label>
             </div>
           </form>
