ff853b7212d87aa41849ea6c110003daaf4741d8
Egor Rumyantsev <RumyantsevGT@ya.ru>
2019-05-16 01:03:38 +0300

    Update index.js

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -33,7 +33,25 @@ class About extends Component {
     return (
       <div>
       
-         <Username username={this.state.userId} />{this.state.userId} 
+         <ul className="plain">
+                {this.props.members.map((member, index) => (
+                  <li key={`user-${index}`}>
+                    <Username username={member.username} />
+                    <span className="icon-container">
+                      {member.id === this.props.userId &&
+                        <span data-toggle="tooltip" data-placement="bottom" title="Me" className="me-icon-wrap">
+                          <User className="me-icon" />
+                        </span>
+                      }
+                      {member.isOwner &&
+                        <span data-toggle="tooltip" data-placement="bottom" title="Owner" className="owner-icon-wrap">
+                          <Star className="owner-icon" />
+                        </span>
+                      }
+                    </span>
+                  </li>
+                ))}
+              </ul>
          
         <br />
 
