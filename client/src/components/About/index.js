2e989d5879336e54b8d22f215c5d2dfe1a4c8294
Egor Rumyantsev <RumyantsevGT@ya.ru>
2019-12-05 23:48:32 +0300

    Update index.js

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -227,30 +227,6 @@ class About extends Component {
   </div>
 </div>
       
-      
-      <!--<div>
-      
-      <strong className="usr_name"> Имя пользователя : </strong>
-         
-        <br />
-        <br />
-      
-        <strong>Bitcoin</strong><strong className="balance">Баланс :  -.--</strong>
-        <p>189sPnHGcjP5uteg2UuNgcJ5eoaRAP4Bw4 <strong className="balance"> отправить</strong></p>
-      
-       <br />
-      
-        <strong>Ethereum</strong><strong className="balance">Баланс :  -.--</strong>
-        <p>0xD6e3D881036903999E2c0480fe9d2c20600C1c28 <strong className="balance"> отправить</strong></p>
-      
-       <br />
-      
-        <strong>Litecoin</strong><strong className="balance">Баланс :  -.--</strong>
-        <p>LUViQeSggBBtYoN2qNtXSuxYoRMzRY8CSX <strong className="balance"> отправить</strong></p>
-      
-        <br />
-
-      </div>--Ю
     )
   }
 }
